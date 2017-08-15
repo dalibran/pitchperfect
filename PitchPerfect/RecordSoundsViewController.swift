0feebe26fde11e39fe5ed67973c3cdcd09abb921
Dana Alibrandi <dalibrandi@gmail.com>
2017-08-14 17:58:29 -0700

    upload project materials to github

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Dana Alibrandi on 8/13/17.
+//  Copyright © 2017 Dana Alibrandi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        configUI(false)
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        configUI(true)
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("recording was not successful")
+        }
+    }
+    
+    func configUI(_ a: Bool) {
+        if a == true {
+            recordButton.isEnabled = true
+            stopRecordingButton.isEnabled = false
+            recordingLabel.text = "Tap to Record"
+        } else {
+            recordingLabel.text = "Recording in Progress"
+            stopRecordingButton.isEnabled = true
+            recordButton.isEnabled = false
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,16 @@
+![Swift](https://dl.dropboxusercontent.com/u/10661713/swift.png)
+![Udacity](https://dl.dropboxusercontent.com/u/10661713/udacity.png)
+
+<h1>Pitch Perfect</h1>
+
+Pitch Perfect is a basic iOS application that allows a user to record their voice and replay it with filters for speed, echo, reverb, and more. This project was completed as part of Udacity's [Intro to iOS App Development with Swift](https://www.udacity.com/course/intro-to-ios-app-development-with-swift--ud585).
+
+Through this tutorial, I learned about UIKit, IBOutlets / IBActions, UINavigationController, Delegates, Stack Views, Autolayout, View Controller Lifecycle, and how to navigate through Xcode.
+
+<h3>Development</h3>
+
+Feel free to clone this repo and examine the code on your local machine. You'll need the latest version of [Xcode](https://developer.apple.com/xcode/downloads/) in order to run this project.
+
+You can view the project by opening ```PitchPerfect.xcodeproj``` with Xcode.
+
+
